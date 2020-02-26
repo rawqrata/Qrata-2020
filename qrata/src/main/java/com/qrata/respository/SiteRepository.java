9cbf22640b168d91c7affc293d72954c7a0aa745
Nikhil Mahajan <nikhil@insonix.com-at-609152665272>
2020-02-26 17:38:42 +0530

    Empty the repor to add new code

--- qrata/src/main/java/com/qrata/respository/SiteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.qrata.respository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.qrata.models.Site;
-
-public interface SiteRepository extends JpaRepository<Site, Long>{
-
-}
